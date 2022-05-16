fd9ecba9b9c0e073a65b4fba839f85453c993881
Teemo37 <89978094+Teemo37@users.noreply.github.com>
2022-05-16 14:59:10 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -3,18 +3,18 @@
 
 TEST(Acc, testall)
 {
-  Account acc(12312, 450);
+  Account acc(12312, 370);
   
   //check get
-  EXPECT_EQ(acc.GetBalance(), 450);
+  EXPECT_EQ(acc.GetBalance(), 370);
   
   //check lock/unlock
-  EXPECT_THROW(acc.ChangeBalance(50), std::runtime_error);
+  EXPECT_THROW(acc.ChangeBalance(70), std::runtime_error);
   acc.Lock(); 
-  EXPECT_NO_THROW(acc.ChangeBalance(50));
-  EXPECT_EQ(acc.GetBalance(), 500);
+  EXPECT_NO_THROW(acc.ChangeBalance(30));
+  EXPECT_EQ(acc.GetBalance(), 400);
   EXPECT_THROW(acc.Lock(), std::runtime_error);
   acc.Unlock();
-  EXPECT_THROW(acc.ChangeBalance(50), std::runtime_error);
+  EXPECT_THROW(acc.ChangeBalance(37), std::runtime_error);
 }
   
